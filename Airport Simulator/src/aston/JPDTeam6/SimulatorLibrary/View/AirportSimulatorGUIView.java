8cb7de4ee33a59b59313912c0fee343dfbcf62c1
Simon <Simon@10.72.15.208>
2014-03-31 14:08:48 +0100

    uw0tm8

--- Airport Simulator/src/aston/JPDTeam6/SimulatorLibrary/View/AirportSimulatorGUIView.java
+++ Airport Simulator/src/aston/JPDTeam6/SimulatorLibrary/View/AirportSimulatorGUIView.java
@@ -2,6 +2,8 @@ package aston.JPDTeam6.SimulatorLibrary.View;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.TableModel;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
@@ -9,6 +11,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+
+import javax.swing.JTable;
 
 import aston.JPDTeam6.SimulatorLibrary.Simulator;
 
@@ -116,6 +120,12 @@ public class AirportSimulatorGUIView extends GUIview {
 			}
 		});
 		
+		displayButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		
 		graphFrame.pack();
 		graphFrame.setVisible(true);
 	}
@@ -123,9 +133,12 @@ public class AirportSimulatorGUIView extends GUIview {
 	private void displayTable(){
 		final JFrame tableFrame = new JFrame("Airport Table");
 		JButton quitButton = new JButton("Quit");
+		JButton displayButton = new JButton("Display Table");
 		
 		//quitButton.putClientProperty(createFrameString, createFrame);
 		quitButton.setMaximumSize(new Dimension(10, 10));
+		displayButton.setMaximumSize(new Dimension(10, 10));
+		
 		
 		JPanel tablePanel = new JPanel();
 		JPanel buttonsPanel = new JPanel();
@@ -133,6 +146,8 @@ public class AirportSimulatorGUIView extends GUIview {
 		tablePanel.setLayout(new BorderLayout());
 		buttonsPanel.setLayout(new BorderLayout());
 		buttonsPanel.add(quitButton);
+		buttonsPanel.add(displayButton);
+		
 		tableFrame.setLayout(new BorderLayout());
 		tableFrame.add(tablePanel, BorderLayout.NORTH);
 		tableFrame.add(buttonsPanel, BorderLayout.SOUTH);
@@ -144,6 +159,49 @@ public class AirportSimulatorGUIView extends GUIview {
 				JComponent c = (JComponent) e.getSource();
 				JFrame f = (JFrame) c.getClientProperty(c);
 				f.dispose();
+			}
+		});
+		
+		displayButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				TableModel test = new TableModel() {
+					public int getColumnCount() { return 10; }
+			          public int getRowCount() { return 10;}
+			          public Object getValueAt(int row, int col) { return new Integer(row*col); }
+					@Override
+					public void addTableModelListener(TableModelListener arg0) {
+						// TODO Auto-generated method stub
+						
+					}
+					@Override
+					public Class<?> getColumnClass(int arg0) {
+						// TODO Auto-generated method stub
+						return null;
+					}
+					@Override
+					public String getColumnName(int arg0) {
+						// TODO Auto-generated method stub
+						return null;
+					}
+					@Override
+					public boolean isCellEditable(int arg0, int arg1) {
+						// TODO Auto-generated method stub
+						return false;
+					}
+					@Override
+					public void removeTableModelListener(TableModelListener arg0) {
+						// TODO Auto-generated method stub
+						
+					}
+					@Override
+					public void setValueAt(Object arg0, int arg1, int arg2) {
+						// TODO Auto-generated method stub
+						
+					}
+			      };
+			      JTable table = new JTable(test);
+			      JScrollPane scrollpane = new JScrollPane(table);
+				
 			}
 		});
 		

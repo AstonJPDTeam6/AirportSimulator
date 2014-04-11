1fce2ceb3761976dfc08057aa65f8b0ef5b3f098
Simon <Simon@100.71.64.17>
2014-04-11 02:31:58 +0100

    Added more GUI stuff including a demo table.

--- Airport Simulator/src/aston/JPDTeam6/AirportSimulator/View/AirportSimulatorTable.java
+++ Airport Simulator/src/aston/JPDTeam6/AirportSimulator/View/AirportSimulatorTable.java
@@ -3,16 +3,14 @@ package aston.JPDTeam6.AirportSimulator.View;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableColumn;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 
-import javax.swing.JTable;
+import java.util.ArrayList;
 
 import aston.JPDTeam6.AirportSimulator.AirportSimulator;
 import aston.JPDTeam6.SimulatorLibrary.Simulator;
@@ -21,34 +19,62 @@ import aston.JPDTeam6.SimulatorLibrary.View.GUIview;
 
 public class AirportSimulatorTable extends GUIview {
 
-	private JFrame frame;
 	final int blankSpace = 6;
 	private JTable table;
+	private ArrayList<String> string;
 
 	public AirportSimulatorTable(){
+		string = new ArrayList<String>();
+		int j=0;
+		while (j<50){
+
+			string.add("1v1 me");
+			j++;
+		}
 		displayTable();
 	}
 
 	private void displayTable(){
+		table = new JTable((string.size()/3)+2, 3);
 
+		//This section sets the collumn names for the table.
+		table.setValueAt("Tick Num:", 0, 0);
+		table.setValueAt("Event:", 0, 1);
+		table.setValueAt("uw0tm8?:", 0, 2);
+
+		//this section sets the content for the cells of the table.
+		int i = 0;
+		int row = 1;
+		int collumn = 0;
+		while(i<string.size()){
+			table.setValueAt(string.get(i),row,collumn);
+			if(collumn==2){
+				collumn=0;
+				row++;
+			}
+			else{
+				collumn++;
+			}
+			i++;
+		}
+		//this section sets up the rest of the GUI.
 		final JFrame tableFrame = new JFrame("Airport Table");
 		JButton quitButton = new JButton("Quit");
-		JButton displayButton = new JButton("Display Table");
 
-		//quitButton.putClientProperty(createFrameString, createFrame);
 		quitButton.setMaximumSize(new Dimension(10, 10));
-		displayButton.setMaximumSize(new Dimension(10, 10));
-
 
 		JPanel tablePanel = new JPanel();
 		JPanel buttonsPanel = new JPanel();
 
-		tablePanel.setLayout(new BorderLayout());
-		buttonsPanel.setLayout(new BorderLayout());
+		tablePanel.setLayout(new FlowLayout());
+		tablePanel.add(table);
+
+		buttonsPanel.setLayout(new FlowLayout());
 		buttonsPanel.add(quitButton);
-		buttonsPanel.add(displayButton);
 
 		tableFrame.setLayout(new BorderLayout());
+		((JPanel)tableFrame.getContentPane()).setBorder(new 
+				EmptyBorder(blankSpace, blankSpace, blankSpace, blankSpace));
 		tableFrame.add(tablePanel, BorderLayout.NORTH);
 		tableFrame.add(buttonsPanel, BorderLayout.SOUTH);
 
@@ -56,30 +82,14 @@ public class AirportSimulatorTable extends GUIview {
 
 		quitButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				JComponent c = (JComponent) e.getSource();
-				JFrame f = (JFrame) c.getClientProperty(c);
-				f.dispose();
-			}
-		});
-
-		displayButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				JTable table = new JTable(2,0);
-				TableColumn newCol = new TableColumn();
-				newCol.setHeaderValue("Tick Number:");
-				table.addColumn(newCol);
-				TableColumn newCol1 = new TableColumn();
-				newCol1.setHeaderValue("Event");
-				table.addColumn(newCol);
-				TableColumn newCol2 = new TableColumn();
-				newCol2.setHeaderValue("uw0tm8");
-				table.addColumn(newCol);
+				tableFrame.dispose();
 			}
 		});
 
 		tableFrame.pack();
 		tableFrame.setVisible(true);
 	}
+
 
 	public void update(Simulator simulator)
 	{
@@ -89,9 +99,7 @@ public class AirportSimulatorTable extends GUIview {
 		model.addRow(new Object[]{(simulator.getTick()), "...", "1v1 me"});
 	}
 
-	
-	public void end(Simulator simulator)
-	{
-		
+
+	public void end(Simulator simulator){
 	}
 }
---
+++ Airport Simulator/src/aston/JPDTeam6/AirportSimulator/View/testClass.java
@@ -0,0 +1,10 @@
+package aston.JPDTeam6.AirportSimulator.View;
+
+public class testClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AirportSimulatorTable test = new AirportSimulatorTable();
+	}
+
+}
